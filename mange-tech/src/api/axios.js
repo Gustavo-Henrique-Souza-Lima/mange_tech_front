8dc614836792f0a0351f70a38b245a7875db4761
Biel559 <bielcampopiano@gmail.com>
2025-12-02 22:01:05 -0300

    commit para o deploy web

--- mange-tech/src/api/axios.js
+++ mange-tech/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://webappeduu.azurewebsites.net',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
